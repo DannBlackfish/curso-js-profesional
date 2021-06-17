ce93ea6ea0e8a2fdd4cd109c6857fc416518538d
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-17 17:24:43 -0500

    class

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay'
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.ts';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] 
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 
 const playButton = document.querySelector('#playButton');
@@ -10,16 +13,15 @@ playButton.onclick = () => player.togglePlay();
 
 const muteButton = document.querySelector('#muteButton');
 muteButton.onclick = () => {
-    if (player.media.muted) {
-        player.unmute();
-    } else {
-        player.mute();
-    }
-}
-
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').catch(error => {
-        console.log(error.message);
-    });
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
 }
