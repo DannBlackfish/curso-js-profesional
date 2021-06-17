98d9075561b42ec21267c22bb17f006573d7cd7e
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-17 10:37:10 -0500

    IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,8 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [
-    new AutoPlay()
-] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] 
+});
 
 const playButton = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
