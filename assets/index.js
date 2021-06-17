02cc805c3a23b5de2cb63270e0d315c20e2769e7
Daniela Alonzo <monsterjunior@outlook.es>
2021-06-16 20:46:03 -0500

    getters and setters

--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,21 @@ import AutoPlay from './plugins/AutoPlay'
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video, plugins: [
-    //new AutoPlay()
+    new AutoPlay()
 ] });
 
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+    if (player.media.muted) {
+        player.unmute();
+    } else {
+        player.mute();
+    }
+}
+
+/* 
 const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+button.onclick = () => player.togglePlay(); */
